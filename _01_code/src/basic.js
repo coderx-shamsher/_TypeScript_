c097d86f64cce4f64841a79e83a480a7bcb0336f
coderx-shamsher <shamsher000789@gmail.com>
2026-07-24 21:37:24 +0530

    day 1 in TS its js code

---
+++ _01_code/src/basic.js
@@ -0,0 +1,14 @@
+/// how we can create classic function with parameter and return value 
+
+function hello(name){
+   return `Hello ${name}`
+}
+
+
+// calling this function
+console.log(hello("shang_han"));
+
+/// now ager hamne argument value k data type mein tori si bhi garbar kari to function logic mein ager hogi keo k user say maine ek string expect kiya tha... 
+
+console.log(hello(true));  
+
