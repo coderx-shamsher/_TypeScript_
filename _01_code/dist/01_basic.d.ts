81b34872497ca8fbcdfc2c36be6aa5dd3ea6c912
coderx-shamsher <shamsher000789@gmail.com>
2026-07-24 21:43:00 +0530

    all dist complied file of Ts

---
+++ _01_code/dist/01_basic.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=01_basic.d.ts.map
---
+++ _01_code/dist/01_basic.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// function 
+/** function (parameter : data type ) : return type (datatype){
+ *
+ * }
+ *
+ *  */
+function return_fn(Name) {
+    return `Hello ${Name}`;
+}
+// calling function 
+console.log(return_fn("coderTs"));
+// console.log(return_fn(22))  // error because type script type error keo k user string value he pass kr sakta
+// but ager node ki help say run krte ho to yeh code run hoga 
+console.log(return_fn(22));
+//# sourceMappingURL=01_basic.js.map
