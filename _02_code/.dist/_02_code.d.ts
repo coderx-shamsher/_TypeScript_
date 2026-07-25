0f683074181548e2c75a4d42303e7d7bd8c3fb12
coderx-shamsher <shamsher000789@gmail.com>
2026-07-25 22:06:59 +0530

    all the compiled files of ts

---
+++ _02_code/.dist/_02_code.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=_02_code.d.ts.map
---
+++ _02_code/.dist/_02_code.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// basic function with types 
+function testfn(name) {
+    return `Hello ${name} wellcome into cyberSec`;
+}
+// calling function 
+console.log(testfn("john"));
+// basic arrow function
+// a void function jo kuch bhi return nhi krta hai, parameter nhi leta just a normal function  
+const test_fn_ = () => {
+    console.log("this is typescript basic arrow function without returning a single value ");
+};
+console.log();
+test_fn_();
+//# sourceMappingURL=_02_code.js.map
