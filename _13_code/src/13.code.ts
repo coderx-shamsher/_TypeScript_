183b56bd117e7ea70f0c39ee7402da7667ef5df9
coderx-shamsher <shamsher000789@gmail.com>
2026-09-10 22:47:10 +0530

    13 ts code practice code

---
+++ _13_code/src/13.code.ts
@@ -0,0 +1,49 @@
+// import AxiosFetchUsers from "./axois.js";
+// import  from "./axois.js";
+
+import { FetchUserFetch } from "./axois.js";
+
+// 
+console.log();
+
+// axois web request package like fetch() method 
+
+try {
+    console.log("Loading....");
+    // ResponseFnc()
+    // AxiosFetchUsers()
+    FetchUserFetch()
+} catch (error) {
+    console.log(error);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log();
